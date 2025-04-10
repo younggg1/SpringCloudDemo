6e0f0030bbab5eaf34e12b1837723fb613958c81
younggg1 <1064254619@qq.com>
2025-04-10 23:09:04 +0800

    实现负载均衡，新建LoadBalancerTest类测试

--- services/service-order/src/main/java/com/yzh/order/service/impl/OrderServiceImpl.java
+++ services/service-order/src/main/java/com/yzh/order/service/impl/OrderServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,9 +21,12 @@ public class OrderServiceImpl implements OrderService {
     DiscoveryClient discoveryClient;
     @Autowired
     RestTemplate restTemplate;
+    @Autowired //一定导入spring-cloud-starter-loadbalancer依赖
+    LoadBalancerClient loadBalancerClient;
     @Override
     public Order createOrder(Long productId,Long userId){
-        Product product =  getProductFromRemote(productId);
+//        Product product =  getProductFromRemote(productId);
+        Product product =  getProductFromRemoteWithLoadBalancer(productId);
         Order order = new Order();
         order.setId(1L);
         // 总金额
@@ -32,14 +36,28 @@ public class OrderServiceImpl implements OrderService {
         order.setProductList(Arrays.asList(product));
         order.setAddress("北京");
         order.setNickName("张三");
-        return null;
+        return order;
+
     }
+
     private Product getProductFromRemote(Long productId){
         //1.获取商品服务所在的所有机器ip+port
         List<ServiceInstance> instances =  discoveryClient.getInstances("service-product");
         ServiceInstance instance = instances.get(0);
         //远程url地址
         String url = "http://"+instance.getHost()+":"+instance.getPort()+"/product/"+productId;
+        log.info("远程调用商品服务地址："+url);
+        //2.给远程发送请求
+        Product product = restTemplate.getForObject(url,Product.class);
+        return product;
+    }
+
+    //完成负载均衡请求
+    private Product getProductFromRemoteWithLoadBalancer(Long productId){
+        //1.获取商品服务所在的所有机器ip+port
+        ServiceInstance choose = loadBalancerClient.choose("service-product");
+        //远程url地址
+        String url = "http://"+choose.getHost()+":"+choose.getPort()+"/product/"+productId;
         log.info("远程调用商品服务地址："+url);
         //2.给远程发送请求
         Product product = restTemplate.getForObject(url,Product.class);
