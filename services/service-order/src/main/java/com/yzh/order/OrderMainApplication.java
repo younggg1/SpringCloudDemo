afa94f94b9eb0fe01e93e444272958431fe5d4e1
younggg1 <1064254619@qq.com>
2025-04-10 20:59:58 +0800

    新建订单启动类

---
+++ services/service-order/src/main/java/com/yzh/order/OrderMainApplication.java
@@ -0,0 +1,12 @@
+package com.yzh.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OrderMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainApplication.class,args);
+    }
+}
