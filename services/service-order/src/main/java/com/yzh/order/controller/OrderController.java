37369f94e69855347bc0cfc101068df96b1601b4
younggg1 <1064254619@qq.com>
2025-04-11 16:54:03 +0800

    使用配置中心，加上自动刷新@RefreshScope

--- services/service-order/src/main/java/com/yzh/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/yzh/order/controller/OrderController.java
@@ -3,14 +3,25 @@ package com.yzh.order.controller;
 import com.yzh.order.bean.Order;
 import com.yzh.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@RefreshScope
 @RestController
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+    @Value("${order.timeout}")
+    String orderTimeout;
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+    @GetMapping("/config")
+    public String config(){
+        return "order.timeout="+orderTimeout+":order.auto-confirm="+orderAutoConfirm;
+    }
     //创建订单
     @GetMapping("/create")
     public Order createOrder(@RequestParam("productId") Long productId,
