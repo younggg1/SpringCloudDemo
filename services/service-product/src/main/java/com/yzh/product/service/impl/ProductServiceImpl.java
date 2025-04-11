6b9dd004066c0482c818d6a58e0ea5d0067c8a47
younggg1 <1064254619@qq.com>
2025-04-11 21:12:22 +0800

    openfeign-进阶配置-超时配置

--- services/service-product/src/main/java/com/yzh/product/service/impl/ProductServiceImpl.java
+++ services/service-product/src/main/java/com/yzh/product/service/impl/ProductServiceImpl.java
@@ -5,6 +5,7 @@ import com.yzh.product.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -17,7 +18,11 @@ public class ProductServiceImpl implements ProductService {
         product.setProductName("苹果"+productId);
 
         product.setNum(2);
-
+        try {
+            TimeUnit.SECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return product;
     }
 }
