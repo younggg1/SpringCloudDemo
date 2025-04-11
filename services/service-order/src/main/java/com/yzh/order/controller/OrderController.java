cb2fdf41ccc7bbe35a294343df4b12f43a1cd833
younggg1 <1064254619@qq.com>
2025-04-11 23:09:58 +0800

    sentinel-流控规则-流控模式-链路

--- services/service-order/src/main/java/com/yzh/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/yzh/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.yzh.order.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.yzh.order.bean.Order;
 import com.yzh.order.properties.OrderProperties;
 import com.yzh.order.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 //@RefreshScope
+@Slf4j
 @RestController
 public class OrderController {
     @Autowired
@@ -27,5 +30,34 @@ public class OrderController {
                              @RequestParam("userId") Long userId){
         Order order = orderService.createOrder(productId,userId);
         return order;
+    }
+
+    @GetMapping("/seckill")
+    @SentinelResource(value = "seckill-order",fallback = "seckillFallback")
+    public Order seckill(@RequestParam(value = "userId",required = false) Long userId,
+                         @RequestParam(value = "productId",defaultValue = "1000") Long productId){
+        Order order = orderService.createOrder(productId, userId);
+        order.setId(Long.MAX_VALUE);
+        return order;
+    }
+
+    public Order seckillFallback(Long userId,Long productId, Throwable exception){
+        System.out.println("seckillFallback....");
+        Order order = new Order();
+        order.setId(productId);
+        order.setUserId(userId);
+        order.setAddress("异常信息："+exception.getClass());
+        return order;
+    }
+
+    @GetMapping("/writeDb")
+    public String writeDb(){
+        return "writeDb success....";
+    }
+
+    @GetMapping("/readDb")
+    public String readDb(){
+        log.info("readDb...");
+        return "readDb success....";
     }
 }
