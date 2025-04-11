5c7233f1a0cd5e147fde5197c63e66112dbb245e
younggg1 <1064254619@qq.com>
2025-04-11 21:19:18 +0800

    openfeign-进阶配置-重试机制

--- services/service-order/src/main/java/com/yzh/order/config/OrderConfig.java
+++ services/service-order/src/main/java/com/yzh/order/config/OrderConfig.java
@@ -1,6 +1,7 @@
 package com.yzh.order.config;
 
 import feign.Logger;
+import feign.Retryer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +19,9 @@ public class OrderConfig {
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+    @Bean
+    Retryer retryer(){
+        return new Retryer.Default();
     }
 }
