1dc2d0d00c9794bc242664b951b107c21d9916e8
younggg1 <1064254619@qq.com>
2025-04-11 18:38:13 +0800

    openfeign-进阶配置-日志

--- services/service-order/src/main/java/com/yzh/order/config/OrderConfig.java
+++ services/service-order/src/main/java/com/yzh/order/config/OrderConfig.java
@@ -1,5 +1,6 @@
 package com.yzh.order.config;
 
+import feign.Logger;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +13,10 @@ public class OrderConfig {
     RestTemplate restTemplate()
     {
         return new RestTemplate();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
