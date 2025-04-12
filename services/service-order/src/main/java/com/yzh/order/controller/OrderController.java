680a9dc472fe7fd6fd80facd6407e100a3f09e0c
younggg1 <1064254619@qq.com>
2025-04-12 15:54:08 +0800

    gateway-路由-配置规则

--- services/service-order/src/main/java/com/yzh/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/yzh/order/controller/OrderController.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 //@RefreshScope
 @Slf4j
+@RequestMapping("/api/order")
 @RestController
 public class OrderController {
     @Autowired
--- services/service-order/src/main/java/com/yzh/order/feign/ProductFeignClient.java
+++ services/service-order/src/main/java/com/yzh/order/feign/ProductFeignClient.java
@@ -5,6 +5,8 @@ import com.yzh.product.bean.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @FeignClient(value = "service-product",fallback = ProductFeignClientFallback.class) // feign客户端
 public interface ProductFeignClient {
@@ -13,7 +15,7 @@ public interface ProductFeignClient {
     //mvc注解的两套使用逻辑
     //1、标注在Controller上，是接受这样的请求
     //2、标注在FeignClient上，是发送这样的请求
-    @GetMapping("/product/{id}")
+    @GetMapping("/api/product/product/{id}")
     Product getProductById(@PathVariable("id") Long id);
 
 
